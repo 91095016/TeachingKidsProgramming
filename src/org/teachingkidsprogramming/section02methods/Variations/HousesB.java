37a25e4104fbd0be93683b23051878fb9cc03f84
91095016 <31664277+91095016@users.noreply.github.com>
2017-09-18 10:47:14 -0700

    addedHouseB.java

---
+++ src/org/teachingkidsprogramming/section02methods/Variations/HousesB.java
@@ -0,0 +1,91 @@
+package org.teachingkidsprogramming.section02methods.Variations;
+
+import org.teachingextensions.logo.Tortoise;
+import org.teachingextensions.logo.utils.ColorUtils.PenColors;
+import org.teachingextensions.virtualproctor.VirtualProctor;
+
+public class HousesB
+{
+  public static void main(String[] args)
+  {
+    VirtualProctor.setClassName("PalmDigital");
+    VirtualProctor.setName("Rebecca Chang");
+    Tortoise.show();
+    Tortoise.setSpeed(10);
+    Tortoise.setX(200);
+    int height = 40;
+    drawHouseCircleRoof(height);
+    drawHouseFlatRoof(height + 50);
+    drawHousePointyRoof(height + 30);
+    drawHouseSlantRoof(height);
+  }
+  private static void drawHouseFlatRoof(int height)
+  {
+    Tortoise.setPenColor(PenColors.Reds.Red);
+    Tortoise.move(height);
+    drawFlatRoof();
+    Tortoise.move(height);
+    Tortoise.turn(-90);
+    Tortoise.move(20);
+    Tortoise.turn(-90);
+  }
+  private static void drawHousePointyRoof(int height)
+  {
+    Tortoise.setPenColor(PenColors.Reds.Red);
+    Tortoise.move(height);
+    drawPointyRoof();
+    Tortoise.move(height);
+    Tortoise.turn(-90);
+    Tortoise.move(20);
+    Tortoise.turn(-90);
+  }
+  private static void drawHouseSlantRoof(int height)
+  {
+    Tortoise.setPenColor(PenColors.Reds.Red);
+    Tortoise.move(height);
+    drawSlantRoof();
+    Tortoise.move(50);
+    Tortoise.turn(-90);
+    Tortoise.move(20);
+    Tortoise.turn(-90);
+  }
+  private static void drawHouseCircleRoof(int height)
+  {
+    Tortoise.setPenColor(PenColors.Reds.Red);
+    Tortoise.move(height + 10);
+    drawCircleRoof();
+    Tortoise.move(50);
+    Tortoise.turn(-90);
+    Tortoise.move(20);
+    Tortoise.turn(-90);
+  }
+  private static void drawPointyRoof()
+  {
+    Tortoise.turn(45);
+    Tortoise.move(15);
+    Tortoise.turn(90);
+    Tortoise.move(15);
+    Tortoise.turn(45);
+  }
+  private static void drawFlatRoof()
+  {
+    Tortoise.turn(90);
+    Tortoise.move(30);
+    Tortoise.turn(90);
+  }
+  private static void drawSlantRoof()
+  {
+    Tortoise.move(30);
+    Tortoise.turn(135);
+    Tortoise.move(30);
+    Tortoise.turn(45);
+  }
+  private static void drawCircleRoof()
+  {
+    for (int i = 1; i < 180; i++)
+    {
+      Tortoise.move(0.25);
+      Tortoise.turn(1);
+    }
+  }
+}

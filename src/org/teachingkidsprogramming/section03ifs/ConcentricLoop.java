9bd3c25fc561832882347f8471a12a3e80f514a3
91095016 <31664277+91095016@users.noreply.github.com>
2017-10-02 10:59:16 -0700

    finished deepDive03

--- src/org/teachingkidsprogramming/section03ifs/ConcentricLoop.java
+++ src/org/teachingkidsprogramming/section03ifs/ConcentricLoop.java
@@ -15,10 +15,17 @@ public class ConcentricLoop
       Tortoise.move(3);
       Tortoise.turn(1);
       //      If i is divisible by 20 --(HINT: Use mod operator '%') --#7
-      //          Do the following 360 times --#8.1
-      Tortoise.move(1);
-      Tortoise.turn(1);
-      //              Change the width of the line that the tortoise draws to 1/100th of the current line --#9      
+      if (i % 20)
+      {
+      }
+      for (int j = 0; j < 360; j++)
+      {
+        Tortoise.move(1);
+        Tortoise.turn(1);
+      }
+      //              Change the width of the line that the tortoise draws
+      //to 1/100th of the current line --#9      
+      Tortoise.setPenWidth(1 / 100);
       //          End Repeat --#8.2
     }
   }

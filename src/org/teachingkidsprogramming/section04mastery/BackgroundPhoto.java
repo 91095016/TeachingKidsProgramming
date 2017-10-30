da5b6ae79d841c25c0b4ac99ac9ac777dce7efa4
91095016 <31664277+91095016@users.noreply.github.com>
2017-10-30 10:56:39 -0700

    started PentagonCrazy

--- src/org/teachingkidsprogramming/section04mastery/BackgroundPhoto.java
+++ src/org/teachingkidsprogramming/section04mastery/BackgroundPhoto.java
@@ -9,6 +9,7 @@ public class BackgroundPhoto
   {
     //  Set the current picture to this url "https://usatftw.files.wordpress.com/2017/05/spongebob.jpg?w=1000&h=600&crop=1" --#1.0
     //  Set the background image to the current picture--#1.1
+    //  BackgroundPhoto
     Tortoise.show();
     Tortoise.setSpeed(10);
     int side = 2;
@@ -18,7 +19,9 @@ public class BackgroundPhoto
     //     Increase the length of a side by 1 pixel --#3.0
     //     Move the tortoise the length of the current side --#3.1
     //     Set the X position of the tortoise to 555 pixels --#2.0
+    Tortoise.setX(555);
     //     Set the Y position of the tortoise to 65 pixels --#2.1
+    Tortoise.setY(65);
     //     Turn the tortoise 1/3rd of 360 degrees to the right --#4
     //     Turn the tortoise 1 more degree --#5
     //  End Repeat --#7.2
